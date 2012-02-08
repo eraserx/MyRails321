cb947c8e902503060d0879f8393829ed4a7a804c
vagrant <vagrant@vagrant.(none)>
2012-02-08 11:44:44 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyRails321::Application.config.secret_token = '0a374e4c144136b137918cd4aebb458b6f2d45e367f02d7b2afb889ba37c5ec04dff04394312ecdc6d6a30d1aec54b4f9824fb6798a2feda21363ad9ee2f105a'
